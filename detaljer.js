cbabdf2ef765f5814a742dbf959dd88063872948
Stfa75 <u2535501@rts-365.dk>
2026-03-05 13:54:35 +0100

    detaljedesign

--- detaljer.js
+++ detaljer.js
@@ -37,7 +37,7 @@ function renderPokemon(data, speciesData) {
     // Vi leder efter den første indgang, hvor sproget er engelsk ("en")
     const flavorEntry = speciesData.flavor_text_entries.find(entry => entry.language.name === "en");
 
-    // Vi renser teksten for mærkelige tegn som \f og \n, så den ser pæn ud
+    //renser teksten for mærkelige tegn så den ser pæn ud
     const flavor = flavorEntry
         ? flavorEntry.flavor_text.replace(/[\f\n\r]/gm, ' ')
         : "Ingen beskrivelse fundet.";
@@ -55,7 +55,9 @@ function renderPokemon(data, speciesData) {
     const moves = data.moves.slice(0, 3).map(m => m.move.name).join(", ");/* 0,3 betyder at vi tager den første plads og slutter ved den 2 plads(3 i alt)*/
     /*Når man "slicer" (skærer) i et array, det svarer det til at tage en kopi af en bestemt del af din liste, 
     uden at ødelægge den originale liste.* - der slices fordi det er de stats, fra arrayet der skal vises* map deler */
-    const types = data.types.map(t => t.type.name).join(", ");
+    const types = data.types.map(t => `
+    <span class="type-badge ${t.type.name}">${t.type.name}</span>
+`).join('');
 
 
 
@@ -63,39 +65,58 @@ function renderPokemon(data, speciesData) {
     const pokedexString = /*html*/
         `
 <div class="beast">
-
 <div class= "top">
- <img src ="img/arrow_back.svg" alt ="pil tilbage">
- <h2>${name}<h2>
+ <a href="index.html"><img src ="img/arrow_back.svg" alt ="pil tilbage"></a>
+ <h2>${name}<h2></div>
 </div>
 <div class="billede">
+            <img src="img/chevron_left.svg" alt="venstre pil">
             <img src="${image}" alt="billede af ${name}">
+            <img src="img/chevron_right.svg" alt="venstre pil">
             </div>
-<div class= "ability">
-    <h3>${types}</h3>
+            
+     <div class="ability">
+    <div class="type-list">  ${types}
     </div>
-    <p>about</p>
+</div>
+</div>
+
+    <h3 class="text1">about</h3>
 
             <div class="oversigt">
             <div class= weight>
-            <h4>${weight}</h4></div>
-            <div class="height"><h4>${height}</h4></div>
-            <div class="move"><h4>${moves}</h4></div>
-            
+            <img src="img/weight.png" alt="en vægt">
+            <h4>${weight} Weight</h4></div>
+            <div class="height">
+            <img src="img/straighten.svg" alt="lineal">
+            <h4>${height} Height</h4></div>
+            <div class="move"><h4>${moves} Moves</h4></div>
+            </div>
+
             <div class="art">
-            <h3>${flavorEntry}</div>
+            <h3>${flavor}</h3>
+            </div>
             
-            <div class="basestats">
-            <h3>Basestats</h3>
-            <h4>HP ${hitp}</h4>
-            <h4>ATK ${attack}</h4>
-            <h4>DEF ${defend}</h4>
-            <h4>SATK ${satk}</h4>
-            <h4>SDEF${sdef}</h4>
-            <h4>SPD ${spd}</h4>
-            </div>   
-`
+            <table>
+            ${data.stats.map(statObj => `
+                <tr>
+                    <th class="stst-navn">${statObj.stat.name}</th>
+                    <td>${statObj.base_stat}</td>
+                    <td>
+                    <div class="bar"><div class="bar-value"></div></div>
+                    </td>
+
+                    </tr>
+             `).join('')}                        
+           </table>
+
+       
+
+
+       
+         `
     infoSearch.insertAdjacentHTML("afterbegin", pokedexString);
 
 
 }
+
